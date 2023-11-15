4c5d21dbd69c96bd17b1123126bc9efd0b6e9702
angel <angelairon17@gmail.com>
2023-11-15 20:42:50 +0100

    final

--- Armas.py
+++ Armas.py
@@ -11,12 +11,17 @@ class pistola (armas):
     def mostrar_informacion(self):
         print(f"sonido: {self.sonido}, nombre: {self.nombre}, color: {self.color}, daño: {self.daño}")
 
+class francotirador (armas):
+    def __init__(self, sonido, nombre, color, daño):
+        super().__init__(sonido, nombre, color)
+        self.daño = daño
+    def mostrar_informacion(self):
+        print(f"sonido: {self.sonido}, nombre: {self.nombre}, color: {self.color}, daño: {self.daño}")
 
-armap = pistola ("RATATATA RATATATA", "MP30", "azul", "30")
-
+armap = pistola ("RATATATA RATATATA", "MP30", "azul", 30)
+armap = francotirador ("pium", "Tokarev SVT-40", "verde", 150)
 
 armap.mostrar_informacion()
-
 
     
         
